9762afd4d80e1e1e10b7d48441aa9d4d96163a42
fi_van <fivan999@yandex.ru>
2023-02-10 18:41:53 +0300

    fixed flake8 issues

--- shop/catalog/apps.py
+++ shop/catalog/apps.py
@@ -3,5 +3,6 @@ from django.apps import AppConfig
 
 class CatalogConfig(AppConfig):
     """Base configuration class for Catalog application"""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalog'
--- shop/homepage/apps.py
+++ shop/homepage/apps.py
@@ -3,5 +3,6 @@ from django.apps import AppConfig
 
 class HomepageConfig(AppConfig):
     """Base configuration class for Homepage application"""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'homepage'
