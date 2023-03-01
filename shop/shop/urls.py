279f1cf7dbc79a88d262fbca8c2b5b95aa0e92d6
fi_van <fivan999@yandex.ru>
2023-03-01 16:56:01 +0300

    fixes 1

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
 
 
 urlpatterns = [
