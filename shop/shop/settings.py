132eec0a9c4178c2a0d33478124f8911f579476a
fi_van <fivan999@yandex.ru>
2023-03-01 22:31:33 +0300

    fixes

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -141,6 +141,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru'
+LANGUAGES = (
+    ('en', ('English',)),
+    ('ru', ('Russian',)),
+)
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale/')]
+
 
 TIME_ZONE = 'UTC'
 
