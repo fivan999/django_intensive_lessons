4f8d08215eb223bf1464a2bfd9f1cb475d026fb4
fi_van <fivan999@yandex.ru>
2023-03-17 21:53:12 +0300

    form fixes

--- shop/feedback/forms.py
+++ shop/feedback/forms.py
@@ -1,4 +1,9 @@
 import django.forms
+from django.conf import settings
+from django.core.mail import send_mail
+from django.utils.datastructures import MultiValueDict
+
+from feedback.models import Feedback, FeedbackFile, FeedbackUserData
 
 
 class FeedbackForm(django.forms.Form):
@@ -26,3 +31,19 @@ class FeedbackForm(django.forms.Form):
         label='Файл',
         help_text='Загрузите файл'
     )
+
+    def save(self, files: MultiValueDict) -> None:
+        """сохраняем форму"""
+        text = self.cleaned_data['text']
+        user_email = self.cleaned_data['email']
+
+        user = FeedbackUserData.objects.filter(email=user_email)
+        if user:
+            user = user.first()
+        else:
+            user = FeedbackUserData.objects.create(email=user_email)
+
+        feedback = Feedback.objects.create(text=text, user=user)
+
+        for file in files.getlist('files'):
+            FeedbackFile.objects.create(file=file, feedback=feedback)
