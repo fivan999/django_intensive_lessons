baa91a8151f9e7978a0cb1ce211a27bbe0d6dbf7
fi_van <fivan999@yandex.ru>
2023-03-23 06:41:54 +0300

    fixes

--- shop/users/tests.py
+++ shop/users/tests.py
@@ -141,13 +141,14 @@ class UserTests(TestCase):
             self.register_data,
             follow=True
         )
-        last_login_start = ShopUser.objects.get(pk=1).last_login
+        user = ShopUser.objects.get(pk=1)
+        last_login_start = user.last_login
         Client().post(
             reverse('users:login'),
             {'username': username, 'password': password},
             follow=True
         )
-        last_login_end = ShopUser.objects.get(pk=1).last_login
+        last_login_end = user.last_login
         self.assertFalse(last_login_end != last_login_start)
 
     @parameterized.expand(
