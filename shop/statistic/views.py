6032044be367b0c51a3dda96df82df51ef2e7b3c
fi_van <fivan999@yandex.ru>
2023-03-27 20:31:30 +0300

    задание 3 пункт 2

--- shop/statistic/views.py
+++ shop/statistic/views.py
@@ -2,9 +2,7 @@ from catalog.models import Item
 from catalog.views import ItemListView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Prefetch, QuerySet
-
-from rating.models import Rating
+from django.db.models import Count, Q, QuerySet
 
 
 class ListOfRatedUserItems(LoginRequiredMixin, ItemListView):
@@ -14,11 +12,12 @@ class ListOfRatedUserItems(LoginRequiredMixin, ItemListView):
 
     def get_queryset(self) -> QuerySet:
         """получаем нужные записи"""
-        return Item.objects.get_published_items().prefetch_related(
-            Prefetch(
-                'item_rating',
-                queryset=Rating.objects.filter(
-                    user__id=self.request.user.id
+        return Item.objects.get_published_items().prefetch_related().annotate(
+            cnt_rates=Count
+            (
+                'item_rating__id',
+                filter=Q(
+                    item_rating__user__id=self.request.user.id
                 )
             )
-        ).order_by('-item_rating__grade')
+        ).filter(cnt_rates__gt=0).order_by('-item_rating__grade')
