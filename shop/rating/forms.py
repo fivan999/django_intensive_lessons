84a15266a20b6afb5b64b79b0ed365d80b525d50
fi_van <fivan999@yandex.ru>
2023-03-27 13:20:27 +0300

    some fixes beforee passing

--- shop/rating/forms.py
+++ shop/rating/forms.py
@@ -6,15 +6,9 @@ import rating.models
 class RatingForm(forms.ModelForm):
     """форма рейтинга"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['grade'].empty_label = None
-
     class Meta:
         model = rating.models.Rating
-        fields = (
-            rating.models.Rating.grade.field.name,
-        )
+        fields = ('grade',)
         widgets = {
             'grade': forms.widgets.Select(
                 attrs={'class': 'form-select'}
